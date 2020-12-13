ad43603d8c3dd735d116886f43434d80a58f0076
Istvan Andrasi <istvan.andrasi@ericsson.com>
2020-12-13 15:16:04 +0100

    Initalizing commit.

---
+++ Client/src/main/java/DataBaseHandler.java
@@ -0,0 +1,11 @@
+import java.util.logging.Logger;
+
+public class DataBaseHandler {
+    private final static Logger LOGGER = Logger.getLogger(DataBaseHandler.class.getName());
+
+
+    public void addData(){
+        LOGGER.info("Adding new row to database");
+        
+    }
+}
